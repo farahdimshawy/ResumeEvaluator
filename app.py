14114caa2af2e5b77ecb279d243bacdc00c1fa5a
Farah <148490880+farahdimshawy@users.noreply.github.com>
2025-12-19 15:33:43 +0200

    required

--- app.py
+++ app.py
@@ -292,10 +292,10 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# Let's check our models
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
+# # Let's check our models
+# for m in genai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
 
 model = genai.GenerativeModel('gemini-2.5-flash')
 
